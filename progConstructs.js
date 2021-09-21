7acb3910b8cc36f5b91c37d9c35ae16f637a9526
samadhanugale <samugale5555@gmail.com>
2021-09-21 17:15:55 +0530

    Update progConstructs.js
    Changes uploaded

--- progConstructs.js
+++ progConstructs.js
@@ -5,3 +5,15 @@ if(age>=18){
 else{
     document.write("Child : Restricted");
 }
+
+//While
+var n = parseInt(window.prompt("Enter Number"));
+var j = 0;
+while(j<=n){
+    document.write("<br>"+j);
+    j++;
+}
+
+for(var k=0;k<=12;k++){
+    document.write("<br>This is :"+k );
+}
