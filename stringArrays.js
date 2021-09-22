413badefaabdf12befdae90ca7d77052b14e548b
samadhanugale <samugale5555@gmail.com>
2021-09-22 11:32:55 +0530

    Array added

--- stringArrays.js
+++ stringArrays.js
@@ -7,3 +7,27 @@ console.log(company);
 let comp1 = '          This     is      for   trim     ';
 console.log(comp1.trim());
 
+
+let charArray = ['h','e','l','l','o'];
+
+let mixArray = [123,"Text",7782.98,"THis"];
+let arraynew = new Array(5);
+
+// charArray.pop();
+// charArray.push(12);
+
+// document.write(charArray);
+// document.write("<br");
+
+let salary1 = [111,2222,3333,4444,5555];
+//document.write(salary1);
+document.write(salary1.slice(2,4));
+
+document.write("<br");
+document.write(mixArray);
+document.write("<br");
+document.write(arraynew);
+document.write("<br");
+document.write(charArray);
+document.write("<br");
+
